3e707dc96ec40259a4fb65020dcf6c0175fe30ba
Sanu <sanu@stream.space>
2021-01-23 21:34:55 +0530

    my first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,43 @@
  */
 
 module.exports = {
+  /* Your site meta data */
+  siteMetadata: {
+    title: "gatsby_title",
+    siteUrl: "https://#",
+    description: "a starter kit",
+  },
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-fontawesome-css",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/assets/images/firstService.png",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-prefetch-google-fonts",
+      options: {
+        fonts: [
+          {
+            family: "Nunito",
+            variants: ["100","300","400","600"],
+          },
+        ],
+        display: "swap",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+  ],
 }
